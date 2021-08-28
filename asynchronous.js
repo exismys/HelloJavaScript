251340bfe3a1397c33ca83cfe4c7d9587b8a2259
exismys <riteshzeronity@gmail.com>
2021-08-28 06:49:34 +0530

    Initial commit

---
+++ asynchronous.js
@@ -0,0 +1,9 @@
+let callbackFunction = function() {
+    console.log("This is a callback function");
+}
+let sayName = function(name) {
+    console.log(name);
+}
+
+setTimeout(callbackFunction, 1000);
+sayName("Exismys"); // This line does not wait for the previous setTimeout call to be completed
---
+++ object.js
@@ -0,0 +1,41 @@
+// function as a constructor
+function Rabbit(name, type) {
+    this.name = name;
+    this.type = type;
+}
+
+let bunny = new Rabbit("bunny", "cute");
+console.log(bunny);
+
+// class notation
+class Dog {
+    constructor(type) {
+        this.type = type;
+    }
+    speak(line) { // this is in Dog's prototype property
+        console.log(`The ${this.type} dog says ${line}`)
+    }
+}
+
+let cuteDog = new Dog("cute");
+cuteDog.speak("woof");
+
+// Object.create()
+
+let simpleObject = {
+    aFunction: function() {
+        console.log("function inside simpleObject");
+    }
+}
+
+let aNewObject = Object.create(simpleObject);
+console.log(aNewObject.aFunction()); // function inside simpleObject
+
+// Parameter.length
+route = [0, 1, 2, 3, 4]
+function routeRobot(state, memory) {
+    if (memory.length == 0) memory = route;
+    console.log(memory);
+}
+
+routeRobot(0, []);
