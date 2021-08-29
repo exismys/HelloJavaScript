f5ee621c5349533851bf32750e5f02ad9f327202
exismys <riteshzeronity@gmail.com>
2021-08-29 11:53:20 +0530

    added new files

--- asynchronous.js
+++ asynchronous.js
@@ -7,3 +7,8 @@ let sayName = function(name) {
 
 setTimeout(callbackFunction, 1000);
 sayName("Exismys"); // This line does not wait for the previous setTimeout call to be completed
+
+/*
+Exismys
+This is a callback function
+*/
---
+++ promise.js
@@ -0,0 +1,2 @@
+let fifteen = Promise.resolve(15); // creating promise through Promise.resolve
+fifteen.then(data => console.log(data)); // 15
