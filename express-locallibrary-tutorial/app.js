32f3592e7f37a454672b8c7ea8a8c1b744121571
exismys <riteshzeronity@gmail.com>
2021-09-07 21:19:52 +0530

    added database and set up the routes and controllers

--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -6,8 +6,19 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog');
+
+require("dotenv").config();
 
 var app = express();
+
+//Set up mongoose connection
+var mongoose = require('mongoose');
+let password = process.env.mongodb_password;
+var mongoDB = 'mongodb+srv://exismys:' + password + '@cluster0.ikahw.mongodb.net/local_library?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
